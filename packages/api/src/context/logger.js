68d93a938ca3361fbbd2ec6ba751251f355234f4
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-08-15 22:32:57 -0300

    feat(api): add context configuration

---
+++ packages/api/src/context/logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston')
+const { combine, timestamp, json, prettyPrint } = winston.format
+
+exports.createLogger = (context) => {
+  const { config } = context
+
+  /* istanbul ignore next */
+  const format = config.NODE_ENV === 'production'
+    ? combine(timestamp(), json())
+    : combine(timestamp(), prettyPrint())
+
+  return winston.createLogger({
+    level: config.LOG_LEVEL,
+    format,
+    transports: [
+      new winston.transports.Console()
+    ],
+    silent: config.NODE_ENV === 'test'
+  })
+}
