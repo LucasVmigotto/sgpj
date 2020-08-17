7b6a3a3b0ea22318e93ba8d27af30f5ba2d7feb6
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-08-17 10:40:43 -0300

    feat(api): add login query

--- packages/api/src/models/User.js
+++ packages/api/src/models/User.js
@@ -1,10 +1,11 @@
 const { gql } = require('apollo-server-express')
+const { cipher } = require('../utils')
 
 const typeDefs = gql`
   type User {
     userId: ID!
     email: String!
-    password: String!
+    password: String
   }
 
   type UserList {
@@ -16,7 +17,52 @@ const typeDefs = gql`
     email: String!
     password: String!
   }
+
+  extend type Query {
+    login(input: UserInput!): Lawyer!
+  }
 `
+
+const resolvers = {
+  Query: {
+    async login (_, { input }, { knex }) {
+      const [{ user_id: userId }] = await knex('user')
+        .select('user_id')
+        .where({
+          ...input,
+          password: cipher(input.password)
+        })
+      if (!userId) {
+        throw new Error('Email or Password invalid')
+      }
+      const [data] = await knex('lawyer')
+        .select(
+          'lawyer.lawyer_id',
+          'lawyer.name',
+          'lawyer.roles',
+          'lawyer.create_at',
+          'lawyer.update_at',
+          'user.user_id',
+          'user.email'
+        )
+        .where({ 'lawyer.user_id': userId })
+        .join('user', { 'lawyer.user_id': 'user.user_id' })
+      return {
+        lawyerId: data.lawyer_id,
+        name: data.name,
+        roles: data.roles,
+        createAt: data.create_at,
+        updateAt: data.update_at,
+        user: {
+          userId: data.user_id,
+          email: data.email
+        }
+      }
+    }
+  }
+}
+
 module.exports = {
-  typeDefs
+  typeDefs,
+  resolvers
 }
