86622a0806c5f270343f34b985fc5dbc74be6d36
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-08-18 19:42:29 -0300

    feat(api): update db model
    Add to lawyer model the oab column

---
+++ packages/api/migrations/20200818193211_lawyer_3.js
@@ -0,0 +1,18 @@
+exports.up = async function (knex) {
+  await knex.schema.alterTable('lawyer', t => {
+    t.string('oab', 7)
+    t.unique('oab')
+  })
+  await knex('lawyer')
+    .update({ 'oab': '1234567' })
+    .where({ lawyer_id: 1 })
+  await knex.schema.alterTable('lawyer', t => {
+      t.string('oab', 7).notNullable().alter()
+  })
+}
+
+exports.down = async function (knex) {
+  await knex.schema.alterTable('lawyer', t => {
+    t.dropColumn('oab')
+  })
+}
