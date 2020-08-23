ced58cdbcd9d38a660e7c046dbf7fab54bbb9519
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-08-22 23:27:37 -0300

    test(api): add Utils and Security unit tests

---
+++ packages/api/test/utils/Utils.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('../utils')
+const {
+  UserTypes,
+  defineType,
+  userInRoles,
+} = require('../../src/utils')
+
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+describe('Utils', function () {
+  describe('defineType', function () {
+    it('return LAWYER', function () {
+      expect(defineType('LAWYER', 0))
+        .to.be.deep.equal({ lawyer_id: 0 })
+    })
+    it('return CLIENT', function () {
+      expect(defineType('CLIENT', 0))
+        .to.be.deep.equal({ client_id: 0 })
+    })
+    it('return LAWSUIT', function () {
+      expect(defineType('LAWSUIT', 0))
+        .to.be.deep.equal({ law_suit_id: 0 })
+    })
+    it('return ERROR', function () {
+      expect(function () {
+        defineType('ERROR', 0)
+      }).to.throw(Error, /Invalid option selected/)
+    })
+  })
+  describe('userInRoles', function () {
+    const user = role => ({ roles: [role] })
+    it('true - default', function () {
+      expect(userInRoles(user('LAWYER')))
+        .to.be.true
+    })
+    it('true', function () {
+      expect(userInRoles(user('ADMIN'), UserTypes.LAWYER))
+        .to.be.true
+    })
+    it('false', function () {
+      expect(userInRoles(user('OTHER'), UserTypes.LAWYER))
+        .to.be.false
+    })
+  })
+})
