7edf6e7aec126bf13c15c9c6f2e5a3f7aef68c49
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-08-22 21:56:05 -0300

    feat(api): update LawSuit GraphQL Playground
    Adjust the LawSuit's Playground tab example to the new structure
    Add token query variables to the LawSuit tab

--- packages/api/src/tabs/law-suit.js
+++ packages/api/src/tabs/law-suit.js
@@ -1,5 +1,10 @@
+const config = require('../config')
+
 module.exports = ({ endpoint, readFile }) => ({
   endpoint,
   name: 'Law Suit',
-  query: readFile('./law-suit.gql')
+  query: readFile('./law-suit.gql'),
+  variables: `{
+    "lawyer": "${config.PLAYGROUND_TOKEN_LAWYER}"
+  }`
 })
